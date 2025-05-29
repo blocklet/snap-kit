4fa318ce45e226a14c4d54893bae697cf4b8046c
zhiyuan <coffce404@outlook.com>
2025-05-29 11:16:37 +0800

    doc: enhance docs

--- packages/crawler/src/config.ts
+++ packages/crawler/src/config.ts
@@ -31,9 +31,9 @@ export const config: Config = {
 
   dataDir: process.env.BLOCKLET_DATA_DIR!,
   appDir: process.env.BLOCKLET_APP_DIR! || process.cwd(),
-  appUrl: process.env.BLOCKLET_APP_URL!,
+  cacheDir: process.env.BLOCKLET_CACHE_DIR! || process.cwd(),
+  appUrl: process.env.BLOCKLET_APP_URL! || '/',
   puppeteerPath: process.env.PUPPETEER_EXECUTABLE_PATH!,
-  cacheDir: process.env.BLOCKLET_CACHE_DIR!,
 
   // cron
   siteCron: {
