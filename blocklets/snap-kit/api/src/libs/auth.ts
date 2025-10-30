0547b14b6bc94afa1d42a878897e40dec32042f2
lixl <lixianling@arcblock.io>
2025-10-30 16:46:02 +0800

    feat: use a more secure signature mechanism 

--- blocklets/snap-kit/api/src/libs/auth.ts
+++ blocklets/snap-kit/api/src/libs/auth.ts
@@ -1,14 +1,14 @@
 import AuthStorage from '@arcblock/did-auth-storage-nedb';
-import getWallet from '@blocklet/sdk/lib/wallet';
-import WalletAuthenticator from '@blocklet/sdk/lib/wallet-authenticator';
-import WalletHandler from '@blocklet/sdk/lib/wallet-handler';
+import { getWallet } from '@blocklet/sdk/lib/wallet';
+import { WalletAuthenticator } from '@blocklet/sdk/lib/wallet-authenticator';
+import { WalletHandlers } from '@blocklet/sdk/lib/wallet-handler';
 import path from 'path';
 
 import env from './env';
 
 export const wallet: any = getWallet();
 export const authenticator = new WalletAuthenticator();
-export const handlers = new WalletHandler({
+export const handlers = new WalletHandlers({
   authenticator,
   tokenStorage: new AuthStorage({
     dbPath: path.join(env.dataDir, 'auth.db'),
